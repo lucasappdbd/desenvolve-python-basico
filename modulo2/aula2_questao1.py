6106209439d5c0631e21b6a6b33460dd892003a6
lucasappdbd <lucas.pinto@pdbomdespacho.com.br>
2025-01-07 20:04:55 -0300

    Exercicios - Tipos de Dados

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,9 @@
+x = 5
+y = 2
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+z = x / y
+print(z)
+print(type(z))
---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,12 @@
+texto = "O resultado é:"
+x = 10
+y = 3.5
+z = x + y
+print(texto)
+print(type(texto))
+print(x)
+print(type(x))
+print(y)
+print(type(y))
+print(z)
+print(type(z))
